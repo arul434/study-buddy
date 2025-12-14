419e1ec06b1b3054d26e8411ded92fdb81983e8d
arul.dsacoe@gmail.com <arul.dsacoe@gmail.com>
2025-12-14 03:04:14 +0000

    refactor: update PydanticOutputParser import path and requirements.

--- src/generator/question_generator.py
+++ src/generator/question_generator.py
@@ -1,4 +1,4 @@
-from langchain.output_parsers import PydanticOutputParser
+from langchain_core.output_parsers import PydanticOutputParser
 from src.models.question_schemas import MCQQuestion,FillBlankQuestion
 from src.prompts.templates import mcq_prompt_template,fill_blank_prompt_template
 from src.llm.groq_client import get_groq_llm
